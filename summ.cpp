9522cd52704784b03885ea4ef6d8249571282f06
andilekeev <91978492+andilekeev@users.noreply.github.com>
2023-11-18 15:52:09 +0300

    Create summ.cpp

---
+++ summ.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string input;
+    int numbers[5] = {0};
+    int sum = 0;
+
+    std::cout << "Введите строку: ";
+    std::cin >> input;
+    
+    // Проверка, что длина введенной строки меньше 10
+    // if (input.length() >= 10) {
+    //     std::cout << "Некорректная длина строки!" << std::endl;
+    //     return 0;
+    // }
+    
+    // Анализируем строку и извлекаем числа
+    int count = 0;
+    for (char c : input) {
+        if (isdigit(c)) { // Проверка, является ли символ числом
+            numbers[count++] = c - '0'; // Преобразуем символ в числовое значение и добавляем в массив
+        }
+    }
+    
+    // Суммируем числа
+    for (int i = 0; i < 5; i++) {
+        sum += numbers[i];
+    }
+    
+    std::cout << "Сумма чисел: " << sum << std::endl;
+
+    return 0;
+}
